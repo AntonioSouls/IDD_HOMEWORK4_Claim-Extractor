d6be7facc64f90681fb3709e9cd13d23b3a3cfaf
AntonioSouls <tonyplanza1@gmail.com>
2025-01-07 16:06:08 +0100

    Updated .gitignore to avoid pycache versioning and changed some logic in ClaimExtractor

--- code/task1/ClaimExtractor.py
+++ code/task1/ClaimExtractor.py
@@ -6,14 +6,17 @@ import time
 
 
 """ ..................... Parte che definisce l'interazione con il modello ..................................................................."""
+
 # Definisce la posizione in cui è memorizzato il comando da dare al modello
 prompt = "".join(open("data/messageForModel.txt","r").readlines())
+
 
 # Definisce l'API da contattare, quindi il modello con cui si comunica
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key="API-KEY",
+  api_key="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",
 )
+
 
 # Funzione che invia messaggi all'LLM e riceve le risposte
 def ask_chatbot(table_content, max_retries, wait_time):
@@ -53,6 +56,7 @@ def ask_chatbot(table_content, max_retries, wait_time):
 
 
 """ .......................... Parte che specifica il comportamento per estrarre i Claim ed interrogare il modello ..........................."""
+
 # Funzione che controlla se una risposta JSON è completa.
 def is_response_complete(response):
     try:
@@ -69,6 +73,7 @@ def is_response_complete(response):
         print(f"JSON incompleto o invalido: {e}")
         return False
 
+
 # Funzione che recupera una risposta incompleta chiedendo al modello di continuare
 def recover_incomplete_response(incomplete_response, table_content):
     
@@ -79,25 +84,25 @@ def recover_incomplete_response(incomplete_response, table_content):
     return ""
 
 
-# # Funzione che riformatta e pulisce la risposta del modello
-# def format_claims(raw_response):
-#     try:
-#         # Utilizza una regex per trovare il JSON nella raw_response
-#         json_match = re.search(r'\{.*\}', raw_response, re.DOTALL)
-#         if not json_match:
-#             raise ValueError("No JSON structure found in the raw response.")
+# Funzione che riformatta e pulisce la risposta del modello
+def format_claims(raw_response):
+    try:
+        # Utilizza una regex per trovare il JSON nella raw_response
+        json_match = re.search(r'\{.*\}', raw_response, re.DOTALL)
+        if not json_match:
+            raise ValueError("No JSON structure found in the raw response.")
         
-#         # Estrai il JSON e caricalo come dizionario
-#         claims_data = json.loads(json_match.group(0))
+        # Estrai il JSON e caricalo come dizionario
+        claims_data = json.loads(json_match.group(0))
         
-#         # Verifica che il risultato sia strutturato come un dizionario
-#         if not isinstance(claims_data, dict):
-#             raise ValueError("Extracted data is not a dictionary.")
+        # Verifica che il risultato sia strutturato come un dizionario
+        if not isinstance(claims_data, dict):
+            raise ValueError("Extracted data is not a dictionary.")
         
-#         return claims_data
-#     except Exception as e:
-#         print(f"Error processing claims: {e}")
-#         return {}
+        return claims_data
+    except Exception as e:
+        print(f"Error processing claims: {e}")
+        return {}
 
 
 # Funzione principale che per ogni tabella chiede al modello di estrarne i claims
@@ -124,10 +129,13 @@ def claim_extractor(cartella_sorgente):
                 output_file.write(raw_response)
             time.sleep(10)
         
-           
-        # file_claims = format_claims(raw_response)
+    # for txt_file in os.listdir("data/claims/txt"):
+    #     txt_file_path = os.path.join("data/claims/txt", txt_file)
+    #     with open(txt_file_path, 'r', encoding="utf-8") as file:
+    #         content = file.read()
+    #     file_claims = format_claims(content)
 
-        # Json_file_sanitized = JSON_File.replace(".json", "_claims.json")
-        # with open(f"data/claims/json/{Json_file_sanitized}", 'w', encoding='utf-8') as file:
-        #     json.dump(file_claims, file, indent=4)
+    #     Json_file_sanitized = txt_file.replace(".txt", "_claims.json")
+    #     with open(f"data/claims/json/{Json_file_sanitized}", 'w', encoding='utf-8') as file:
+    #         json.dump(file_claims, file, indent=4)
     return
