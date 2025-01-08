541e835bd4472c72faa8586701f262e89571f3e4
AntonioSouls <tonyplanza1@gmail.com>
2025-01-08 13:54:56 +0100

    Solved the incomplete response problem and upgraded MainFunction

--- code/main.py
+++ code/main.py
@@ -1,8 +1,13 @@
 from task1 import TableConverter, ClaimExtractor
+import time
 
 # Funzione principale che orchestra l'intera esecuzione del programma
 def main():
+    start_time = time.time()
     TableConverter.table_extractor("data/extraction","data/conversions", "data/images")
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Conversion Completed in {execution_time:.6f} secondi")
     ClaimExtractor.claim_extractor("data/conversions")
 
 # Funzione starter dello script che fa partire la funzione principale
